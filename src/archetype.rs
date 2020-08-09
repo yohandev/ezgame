eda59fa3f97f0825f2b80a39d2119ccaad1f8989
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-08-09 02:21:11 -0700

    archetype meta via rc

--- src/archetype.rs
+++ src/archetype.rs
@@ -3,13 +3,15 @@ use std::cell::UnsafeCell;
 use std::alloc::Layout;
 use std::ptr::NonNull;
 use std::any::TypeId;
+use std::rc::Rc;
 
-use crate::EntId;
+use crate::{ EntId, Component };
 
 pub struct Archetype
 {
-    /// meta-data about the components' types stored in this archetype
-    meta: HashMap<TypeId, CmpMeta>,
+    /// meta-data about this `Archetype`, which is shared with its `ArchetypeChunk`
+    /// children
+    meta: Rc<ArchetypeMeta>,
     /// all chunks in this archetype. the collection can be expanded but is
     /// never shrunk, therefore elements are 'pinned' and an index can safely
     /// reference a chunk
@@ -18,19 +20,14 @@ pub struct Archetype
     ///
     /// TODO: shared component to free chunk map of type `HashMap<..., Vec<ArchetypeChunkIndex>>
     free: Vec<ArchetypeChunkIndex>,
-    /// (cached) max entities that can be stored in a single chunk within
-    /// this archetype
-    ///
-    /// a chunk stores the exact same amount of components between varying
-    /// types, with no overlap inside roughly 16kb
-    max: usize,
-    /// (cached) layout for every chunk allocations for this archetype
-    layout: Layout,
 }
 
-/// represents a single chunk in an archetype
-struct ArchetypeChunk
+/// a single, 16kb chunk in an archetype
+pub struct ArchetypeChunk
 {
+    /// meta-data about this chunk's parent `Archetype`, which is shared with
+    /// it too
+    meta: Rc<ArchetypeMeta>,
     /// ~16kb chunk of packed `EntId` + `impl Component`
     ///
     /// `*data.get()[0]` is the first entity ID, therefore, `data.get()`
@@ -46,9 +43,22 @@ struct ArchetypeChunk
     data: UnsafeCell<NonNull<u8>>,
     /// number of entities currently stored in this chunk
     len: usize,
-    /// (cached) layout for every chunk allocations for this archetype
+}
+
+/// meta-data about an archetype, which is shared(via `Rc`) between a parent `Archetype`
+/// and its `ArchetypeChunk` children. this is caclulated once and never altered in
+/// the `Archetype::new` constructor
+struct ArchetypeMeta
+{
+    /// meta-data about the components' types stored in this archetype
+    cmp: HashMap<TypeId, CmpMeta>,
+    /// (cached) max entities that can be stored in a single chunk within
+    /// this archetype
     ///
-    /// this is copied from the parent `Archetype` to be used in its `Drop` implementation
+    /// a chunk stores the exact same amount of components between varying
+    /// types, with no overlap inside roughly 16kb
+    max: usize,
+    /// (cached) layout for every chunk allocations for this archetype
     layout: Layout,
 }
 
@@ -100,65 +110,64 @@ impl Archetype
     /// creates a new archetype from a list of (maybe unsorted) types
     pub fn new(mut ty: Vec<TypeMeta>) -> Self
     {
-        // sort types by alignment(greatest to least)...
-        // note: this might not be necesarry...
-        ty.sort_unstable();
-
-        // alignment of chunks(EntId, because `*self.data.get()` starts with entity IDs
-        let align = std::mem::align_of::<EntId>();
-
-        // size, in bytes, of all components + EntId for one entity excluding padding
-        let size = std::mem::size_of::<EntId>() + ty
-            .iter()
-            .fold(0, |acc, n| acc + n.size);
-        // max entities that can be stored in this chunk
-        let max = Self::CHUNK_SIZE / size;
-        // `alloc`: size, in bytes, of the allocation per chunk. it over-allocates slightly
-        // to have space for padding, but ends up roughly equal to `16kb`
-        // `meta`: meta info about the components within this archetype
-        let (alloc, meta) =
+        // archetype meta...
+        let meta =
         {
-            // incrementing allocation size...
-            // ...start with entity IDs
-            let mut alloc = std::mem::size_of::<EntId>() * max;
-            // meta...
-            // ...will have exact same size as `ty` argument
-            let mut meta = HashMap::with_capacity(ty.len());
+            // sort types by alignment(greatest to least)...
+            // note: this might not be necesarry...
+            ty.sort_unstable();
 
-            for t in ty
+            // alignment of chunks(EntId, because `*self.data.get()` starts with entity IDs
+            let align = std::mem::align_of::<EntId>();
+
+            // size, in bytes, of all components + EntId for one entity excluding padding
+            let size = std::mem::size_of::<EntId>() + ty
+                .iter()
+                .fold(0, |acc, n| acc + n.size);
+            // max entities that can be stored in this chunk
+            let max = Self::CHUNK_SIZE / size;
+            // `alloc`: size, in bytes, of the allocation per chunk. it over-allocates slightly
+            // to have space for padding, but ends up roughly equal to `16kb`
+            // `meta`: meta info about the components within this archetype
+            let (alloc, cmp) =
             {
-                // padding for alignment(increment alloc_size)
-                alloc += (t.align - (alloc % t.align)) % t.align;
+                // incrementing allocation size...
+                // ...start with entity IDs
+                let mut alloc = std::mem::size_of::<EntId>() * max;
+                // meta...
+                // ...will have exact same size as `ty` argument
+                let mut meta = HashMap::with_capacity(ty.len());
 
-                // add to meta
-                meta.insert(t.id, (t, alloc));
-                
-                // component data(increment alloc_size)
-                alloc += t.size * max;
-            }
+                for t in ty
+                {
+                    // padding for alignment(increment alloc_size)
+                    alloc += (t.align - (alloc % t.align)) % t.align;
 
-            (alloc, meta)
+                    // add to meta
+                    meta.insert(t.id, (t, alloc));
+                    
+                    // component data(increment alloc_size)
+                    alloc += t.size * max;
+                }
+
+                (alloc, meta)
+            };
+
+            // layout for a chunk allocation within this archetype
+            let layout = Layout::from_size_align(alloc, align).unwrap();
+
+            // return the archetype meta...
+            ArchetypeMeta { cmp, max, layout }
         };
 
-        // layout for a chunk allocation within this archetype
-        let layout = Layout::from_size_align(alloc, align).unwrap();
-        
+        // use a shared ref, to share with children chunks
+        let meta = Rc::new(meta);
         // start with no chunks...
         let chunks = vec![];
         // ...no chunks therefore no free chunks
         let free = vec![];
 
-        Self { meta, max, layout, chunks, free }
-    }
-
-    /// returns a slice of components via `TypeId`. see `Archetype::components`
-    pub fn components_dyn(&self, ty: TypeId) -> &[u8]
-    {
-        let meta = self.meta
-            .get(&ty)
-            .expect("attempting to access components not within this archetype!");
-
-        todo!()
+        Self { meta, chunks, free }
     }
 
     /// create a new empty chunk with no shared components and add it to
@@ -166,7 +175,7 @@ impl Archetype
     fn new_chunk(&mut self)
     {
         // first get a well-aligned layout
-        let layout = self.layout;
+        let layout = self.meta.layout;
         // make a heap allocation and get the pointer
         let ptr = unsafe
         {
@@ -175,13 +184,57 @@ impl Archetype
         // make a cell out of the pointer
         let data = UnsafeCell::new(NonNull::new(ptr).unwrap());
 
+        // clone the shared ref to the parent `Archetype`'s meta
+        let meta = Rc::clone(&self.meta);
+
         // chunk starts empty(no entities)
         let len = 0;
 
         // mark the new chunk as free(which it will be)
         self.free.push(self.chunks.len());
         // append the chunk to this archetype
-        self.chunks.push(ArchetypeChunk { data, len, layout });
+        self.chunks.push(ArchetypeChunk { meta, data, len });
+    }
+}
+
+impl ArchetypeChunk
+{
+    /// see `ArchetypeChunk::components`
+    ///
+    /// the _dyn flavor of functions is for scripting languages, where runtime
+    /// types are used
+    pub fn components_dyn(&self, ty: TypeId, size: usize) -> &[u8]
+    {
+        // meta-data about the component being accessed
+        let meta = self.meta.cmp
+            .get(&ty)
+            .expect("attempting to access components not within this archetype!");
+
+        // pointer to the start of component being accessed
+        let ptr = unsafe { (*self.data.get()).as_ptr().add(meta.1) };
+
+        // create slice
+        unsafe { std::slice::from_raw_parts(ptr, size * self.len) }
+    }
+
+    /// returns a slice of components within this `ArchetypeChunk`. the `T` parameter
+    /// must `impl Component` AND be stored within this archetype, else the function
+    /// will panic.
+    ///
+    /// the slice returned only contains the occupied components, not the entire capacity:
+    /// `&[T].len() == chunk.len()`
+    pub fn components<T: Component>(&self) -> &[T]
+    {
+        // meta-data about the component being accessed
+        let meta = self.meta.cmp
+            .get(&TypeId::of::<T>())
+            .expect("attempting to access components not within this archetype!");
+
+        // pointer to the start of component being accessed
+        let ptr = unsafe { (*self.data.get()).as_ptr().add(meta.1) as *const T };
+
+        // create slice
+        unsafe { std::slice::from_raw_parts(ptr, self.len) }
     }
 }
 
@@ -191,7 +244,7 @@ impl Drop for ArchetypeChunk
     {
         unsafe
         {
-            std::alloc::dealloc((*self.data.get()).as_ptr(), self.layout);
+            std::alloc::dealloc((*self.data.get()).as_ptr(), self.meta.layout);
         }
     }
 }
